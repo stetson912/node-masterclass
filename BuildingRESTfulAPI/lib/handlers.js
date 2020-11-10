56762b5d49751594af5614ad27f7855303d4c96f
stets <ts912@DESKTOP-K00RLQC.localdomain>
2020-11-10 15:40:45 -0700

    added tokens service per course outline

--- BuildingRESTfulAPI/lib/handlers.js
+++ BuildingRESTfulAPI/lib/handlers.js
@@ -89,14 +89,24 @@ handlers._users.get = function(data,callback){
   //check that the phone number is valid
   var phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
   if(phone){
-    // lookup user data in .read function is data returned from teh read function, not the data from the request handler
-    _data.read('users',phone,function(err,data){
-      if(!err && data){
-        // remove hashed password from user object before returning it to the requester
-        delete data.hashedPassword;
-        callback(200,data);
-      }else {
-        callback(404);
+
+    // get the token from the headers
+    var token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
+    //verify that the header token is valid for the phone number
+    handlers._tokens.verifyToken(token,phone,function(tokenIsValid){
+      if(tokenIsValid){
+        // lookup user data in .read function is data returned from teh read function, not the data from the request handler
+        _data.read('users',phone,function(err,data){
+          if(!err && data){
+            // remove hashed password from user object before returning it to the requester
+            delete data.hashedPassword;
+            callback(200,data);
+          }else {
+            callback(404);
+          }
+        });
+      } else {
+        callback(403,{"Error":"Missing required header token or token is invalid"});
       }
     });
   } else {
@@ -108,7 +118,6 @@ handlers._users.get = function(data,callback){
 // users put
 // required field is phone
 //optional data : firstName, lastName, password (at least one of these must be provided)
-// TODO: only let authenticated user update their own object. don't let them uptade any other object
 handlers._users.put = function(data,callback){
   // check for required field. phone
   var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
@@ -121,29 +130,41 @@ handlers._users.put = function(data,callback){
   if(phone){
     // error if nothing is sent to update
     if(firstName || lastName || password){
-      // lookup the user
-      _data.read('users',phone, function(err,userData){
-        if(!err && data){
-          // update necessary fields
-          if(firstName){
-            // update user first name
-            userData.firstName = firstName;
-          }
 
-          if(lastName){
-            userData.lastName = lastName;
-          }
-          if(password){
-            userData.hashedPassword = helpers.hash(password);
-          }
-          // store new user information
-          _data.update('users',phone,userData,function(err){
-            if(!err){
-              callback(200);
-            } else {
-              callback(500,{"Error":"Could not update the user"});
-            }
-          });
+      // get the token from the headers
+      var token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
+
+      //verify that the header token is valid for the phone number
+      handlers._tokens.verifyToken(token,phone,function(tokenIsValid){
+        if(tokenIsValid){
+          // lookup the user
+          _data.read('users',phone, function(err,userData){
+            if(!err && data){
+              // update necessary fields
+              if(firstName){
+                // update user first name
+                userData.firstName = firstName;
+              }
+
+              if(lastName){
+                userData.lastName = lastName;
+              }
+              if(password){
+                userData.hashedPassword = helpers.hash(password);
+              }
+              // store new user information
+              _data.update('users',phone,userData,function(err){
+                if(!err){
+                  callback(200);
+                } else {
+                  callback(500,{"Error":"Could not update the user"});
+                }
+              });
+        } else {
+          callback(403,{"Error":"Missing required header token or token is invalid"});
+        }
+      });
+
         } else {
           callback(400, {"Error":"The specified user does not exist"});
         }
@@ -166,16 +187,27 @@ handlers._users.delete = function(data,callback){
   //check that the phone number is valid
   var phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
   if(phone){
-    // lookup user data in .read function is data returned from teh read function, not the data from the request handler
-    _data.read('users',phone,function(err,data){
-      if(!err && data){
-        _data.delete('users',phone,function(err){
-          if(!err){
-            callback(200);
-          } else {
-            callback(500,{"Error":"Could not delete user."});
-          }
-        });
+    // get the token from the headers
+    var token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
+
+    //verify that the header token is valid for the phone number
+    handlers._tokens.verifyToken(token,phone,function(tokenIsValid){
+      if(tokenIsValid){
+        // lookup user data in .read function is data returned from teh read function, not the data from the request handler
+        _data.read('users',phone,function(err,data){
+          if(!err && data){
+            _data.delete('users',phone,function(err){
+              if(!err){
+                callback(200);
+              } else {
+                callback(500,{"Error":"Could not delete user."});
+              }
+            });
+      } else {
+        callback(403,{"Error":"Missing required header token or token is invalid"});
+      }
+    });
+
       }else {
         callback(400,{"Error":"Could not fine specified user."});
       }
@@ -183,7 +215,165 @@ handlers._users.delete = function(data,callback){
   } else {
     callback(400,{'Error': 'Missing required field'});
   }
+};
+
+// Tokens handlers
+handlers.tokens = function(data,callback){
+  var acceptableMethods = ["post","get","put","delete"];
+  if (acceptableMethods.indexOf(data.method) > -1){
+    handlers._tokens[data.method](data,callback);
+  } else {
+      callback(405);
+  }
+};
+// Container for tokens submethods
+handlers._tokens = {};
+
+// Tokens - post
+ // required fields are phone and password
+ // optional data : none
+handlers._tokens.post = function(data,callback){
+  var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+  var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+
+  if(phone && password){
+    // lookup user who matches the phone number
+    _data.read('users',phone,function(err, userData){
+      if(!err && userData){
+        // hash the sent password and compare it to the password stored in data/_users
+        var hashedPassword = helpers.hash(password);
+        if(hashedPassword == userData.hashedPassword){
+          // if valid create a new token with random name and expiration date 1 hr in the future
+          var tokenId = helpers.createRandomString(20);
+
+          var expires = Date.now() + (1000 * 60 * 60);
+
+          var tokenObject = {
+            'phone':phone,
+            'id':tokenId,
+            'expires':expires
+          };
+
+          // store token
+          _data.create('tokens',tokenId,tokenObject,function(err){
+            if(!err) {
+              callback(200,tokenObject);
+            } else {
+              callback(500,{"Error":"Could not create/save token"});
+            }
+          });
+        } else {
+          callback(400,{"Error":"Password does not match the specified user\'s stored password"});
+        }
+      } else {
+        callback(400,{"Error":"Could not find specified user"});
+      }
+    });
+  } else {
+    callback(400,{"Error":"Missing required fields."});
+  }
+};
+
+// Tokens - get
+// required data: id
+// optoinal data none
+handlers._tokens.get = function(data,callback){
+  // check that the sent id is valid
+  var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length == 20 ? data.queryStringObject.id.trim() : false;
+  if(id){
+    // lookup user data in .read function is data returned from teh read function, not the data from the request handler
+    _data.read('tokens',id,function(err,tokenData){
+      if(!err && tokenData){
+        callback(200,tokenData);
+      }else {
+        callback(404);
+      }
+    });
+  } else {
+    callback(400,{'Error': 'Missing required field'});
+  }
+};
+
+// Tokens - put
+// required fields : id and extend
+// optional data : none
+// TODO: only let authenticated users extend their token
+handlers._tokens.put = function(data,callback){
+  var id = typeof(data.payload.id) == 'string' && data.payload.id.trim().length == 20 ? data.payload.id.trim() : false;
+  var extend = typeof(data.payload.extend) == 'boolean' && data.payload.extend == true ? true : false;
+  if(id && extend == true){
+    // lookup the token
+    _data.read('tokens',id,function(err,tokenData){
+      if(!err && tokenData){
+        // check if the token has expired
+        if(tokenData.expires > Date.now()){
+          // extend the token 1 hr
+          tokenData.expires = Date.now() + (1000 *60 * 60);
+          _data.update('tokens',id,tokenData,function(err){
+            if(!err) {
+              callback(200);
+            } else {
+              callback(500,{"Error":"Could not update token expiration"});
+            }
+          });
+        } else {
+          callback(400,{"Error":"Token has expired"});
+        }
+      } else {
+        callback(400,{"Error":"Specified token does not exist"});
+      }
+    });
+  } else {
+    callback(400,{"Error":"Missing required fields or fields are invalid"});
+  }
+
 
 };
+
+// Tokens - delete
+// required data : id
+// optional data: none
+handlers._tokens.delete = function(data,callback){
+  //check that the phone number is valid
+  var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length == 20 ? data.queryStringObject.id.trim() : false;
+  if(id){
+    // lookup user data in .read function is data returned from teh read function, not the data from the request handler
+    _data.read('tokens',id,function(err,data){
+      if(!err && data){
+        _data.delete('tokens',id,function(err){
+          if(!err){
+            callback(200);
+          } else {
+            callback(500,{"Error":"Could not delete token."});
+          }
+        });
+      }else {
+        callback(400,{"Error":"Could not find specified token."});
+      }
+    });
+  } else {
+    callback(400,{'Error': 'Missing required field'});
+  }
+};
+
+// verify that a given token id is currently valid for a given user
+handlers._tokens.verifyToken = function(id,phone,callback){
+  // lookup the tokens
+  _data.read('tokens',id,function(err, tokenData){
+    if(!err && tokenData){
+      // check that teh token is for the given user and that it is not expired
+      if(tokenData.phone == phone && tokenData.expires > Date.now()){
+        callback(true);
+      } else {
+        callback(false);
+      }
+    } else {
+      callback(false);
+    }
+  });
+};
+
+
+
 
 module.exports = handlers;
--- BuildingRESTfulAPI/lib/helpers.js
+++ BuildingRESTfulAPI/lib/helpers.js
@@ -30,9 +30,30 @@ helpers.parseJsonToObject = function(str){
   }
 };
 
+// creat a string of random alpha numeric characthers of a given length
+helpers.createRandomString = function(strLength){
+  strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+
+  if(strLength){
+    // Define all possible characters that can make the string
+    var possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    // create final string
+    var str = '';
+    for(i = 1; i <= strLength; i++){
+      // get random character from possibleCharacters string and append to string
+      var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+      str += randomCharacter;
+    }
+    return str;
+
+  } else {
+    return false;
+  }
+};
+
 
 
 
 
 // export the module
-module.exports = helpers
+module.exports = helpers;
